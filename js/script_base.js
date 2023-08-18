3e0c300a5bb13aa08617941f258f015d3b38b102
CPAM76094 <108677578+CPAM76094@users.noreply.github.com>
2023-08-18 11:39:27 +0200

    Create script_base.js

---
+++ js/script_base.js
@@ -0,0 +1,60 @@
+let overlay = document.getElementsByClassName('loading-overlay')[0];
+//overlay.classList.toggle('is-active');
+
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var players_list = ["ytpl-playerAM76"];
+
+function getPlaylistData(playerName) {
+    var apiKey = 'AIzaSyAE5ys6wCOxsBCqQ376izGfylErr9vZDBI';
+    var url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet';
+    var data = {
+        'playlistId': $('#' + playerName).data('pl'),
+        'key': apiKey
+    };
+    
+
+}
+
+
+// generate playlist items once main player has loaded
+function onPlayerReady(event) {
+      //setTimeout(overlay.classList.toggle('is-active'), 40000); 
+      getPlaylistData(event.target.name);     
+}
+
+
+function onYouTubeIframeAPIReady() {
+  
+    for (item in players_list) {
+        players_list[item] = new YT.Player(players_list[item], {
+            playerVars: {
+                listType: 'playlist',
+                list: $('#' + players_list[item]).data('pl'),
+                autoplay: 1,
+                playsinline: 1, 
+                rel: 0, 
+                loop: 1, 
+                autopause: 0, 
+                modestbranding: 0, 
+                enablejsapi: 1, 
+                showinfo: 1,
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+}
+
+function onPlayerStateChange(event) {
+    var currentIndex = event.target.getPlaylistIndex();
+    if(currentIndex) { console.log("Dernier clip :"+currentIndex+"/"+event.target.getPlaylist().length);}
+    //console.log(currentIndex);
+  }
+
